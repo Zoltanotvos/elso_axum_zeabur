1ed378230ca0632704af96b745b45e912e64557b
Zoltanotvos <otvoszolka@gmail.com>
2025-07-23 10:52:25 +0200

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,6 @@ async fn main() {
 
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port.parse().unwrap())).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
